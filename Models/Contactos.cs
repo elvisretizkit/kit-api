bb928582c553ec25531c6224b6f3cb55c661ba98
elvisretizkit <eretiz_kit@gpoasshel.com>
2023-10-11 11:28:09 -0600

    Implementacion Modelo Contactos
    Se crea clase para modelo de Contactos

---
+++ Models/Contactos.cs
@@ -0,0 +1,14 @@
+﻿namespace kit_api.Models
+{
+    public class Contactos
+    {
+        public int Contacto { get; set; }
+        public int Cliente { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Correo { get; set; }
+        public string Puesto { get; set; }
+        public bool Activo { get; set; }
+    }
+}
