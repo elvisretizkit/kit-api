8ffc31e8fc8a6798a85939732f397fa8b2acc4f7
elvisretizkit <eretiz_kit@gpoasshel.com>
2023-10-11 11:54:10 -0600

    Implementacion Modelo Relojes
    Se crea clase para el modelo de Relojes

---
+++ Models/Relojes.cs
@@ -0,0 +1,12 @@
+﻿namespace kit_api.Models
+{
+    public class Relojes
+    {
+        public string Mac { get; set; }
+        public string Serie { get; set; }
+        public int Guardia { get; set; }
+        public string Tipo { get; set; }
+        public string Lector { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
