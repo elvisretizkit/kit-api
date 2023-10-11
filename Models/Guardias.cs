5a06e7a3fa401c6cd4c93dabcf7840dd797402eb
elvisretizkit <eretiz_kit@gpoasshel.com>
2023-10-11 11:50:57 -0600

    Implementación Modelo Guardias
    Se crea clase para modelo de Guardias

---
+++ Models/Guardias.cs
@@ -0,0 +1,14 @@
+﻿namespace kit_api.Models
+{
+    public class Guardias
+    {
+        public int Guardia { get; set; }
+        public int Cliente { get; set; }
+        public int Tipo { get; set; }
+        public int Empleados { get; set; }
+        public int Plataforma { get; set; }
+        public string Version { get; set; }
+        public string Build { get; set; }
+        public DateTime Vencimiento { get; set; }
+    }
+}
