9b3ff5d5257cdd8e1893fd2935a1ca936c4d019b
elvisretizkit <eretiz_kit@gpoasshel.com>
2023-10-11 11:23:03 -0600

    Creacion de modelos Clientes y Tickets
    Se crea estructura de modelos para Clientes y Tickets

---
+++ Models/Clientes.cs
@@ -0,0 +1,23 @@
+﻿namespace kit_api.Models
+{
+    public class Clientes
+    {
+        public int Cliente { get; set; }
+        public string RazonSocial { get; set; }
+        public bool Activo { get; set; }
+        public bool PolizaAnual { get; set; }
+        public string Contacto { get; set; }
+        public string Correo { get; set; }
+        public string NombreCorto { get; set; }
+        public string NombreCotizacion { get; set; }
+        public string Categoria { get; set; } 
+        public string Direccion { get; set; }
+        public string Colonia { get; set; }
+        public string Ciudad { get; set; }
+        public string Estado { get; set; }
+        //TODO: Revisar si hay que eliminar esta propiedad
+        public  string Version { get; set; }
+        public float HorasContrato { get; set; }
+        public string Telefono { get; set; }
+    }
+}
---
+++ Models/Tickets.cs
@@ -0,0 +1,19 @@
+﻿namespace kit_api.Models
+{
+    public class Tickets
+    {
+        public int Ticket { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Tema { get; set; }
+        public int Estatus { get; set; }
+        public int Cliente { get; set; }
+        public int Contacto { get; set; }
+        public int Prioridad { get; set; }
+        public string Asesor { get; set; }
+        public string Categoria { get; set; }
+        public float Tiempo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Facturable { get; set; }
+        public DateTime FechaProgramado { get; set; }
+    }
+}
