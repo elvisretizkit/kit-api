20e60eacfdc012db88dacb571608042e4e3d503e
elvisretizkit <eretiz_kit@gpoasshel.com>
2023-10-11 10:34:10 -0600

    Manejo de errores AsesoresController
    Se agrega el manejo de errores a los métodos del controlador de Asesores

--- Controllers/AsesoresController.cs
+++ Controllers/AsesoresController.cs
@@ -13,40 +13,80 @@ namespace kit_api.Controllers
         [HttpGet]
         public async Task <ActionResult<List<Asesores>>> Get()
         {
-            List<Asesores> asesores = await service.ObtenerAsesores(1);
-            return asesores;
+            try
+            {
+                List<Asesores> asesores = await service.ObtenerAsesores(1);
+                return Ok(asesores);
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return StatusCode(500);
+            }
         }
 
         [HttpGet("{codigoAsesor}")]
         public async Task<ActionResult<Asesores>> Get(string codigoAsesor)
         {
-            var result = await service.ObtenerAsesor(codigoAsesor);
-            return result;
+            try
+            {
+                var result = await service.ObtenerAsesor(codigoAsesor);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return StatusCode(500);
+            }
         }
 
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] Asesores asesor)
         {
-            await service.InsertarAsesor(asesor);
-            return NoContent();
+            try
+            {
+                await service.InsertarAsesor(asesor);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return StatusCode(500);
+            }
         }
 
         [HttpPut("{codigoAsesor}")]
         public async Task<ActionResult> Put(string codigoAsesor, [FromBody] Asesores asesor)
         {
-            if (codigoAsesor != asesor.Asesor)
+            try
             {
-                return BadRequest();
+                if (codigoAsesor != asesor.Asesor)
+                {
+                    return BadRequest();
+                }
+                await service.ActualizarAsesor(asesor);
+                return NoContent();
             }
-            await service.ActualizarAsesor(asesor);
-            return NoContent();
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return StatusCode(500);
+            }
         }
 
         [HttpDelete("{codigoAsesor}")]
         public async Task<ActionResult> Delete(string codigoAsesor)
         {
-            await service.EliminarAsesor(codigoAsesor);
-            return NoContent();
+            try
+            {
+                await service.EliminarAsesor(codigoAsesor);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return StatusCode(500);
+            }
         }
     }
 }
