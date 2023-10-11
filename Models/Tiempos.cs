f31cdd84c3193ed28e80441187264172c74766af
elvisretizkit <eretiz_kit@gpoasshel.com>
2023-10-11 11:59:32 -0600

    Impementacion Modelo Tiempos
    Se crea clase para modelo de tabla tickets_tiempos

---
+++ Models/Tiempos.cs
@@ -0,0 +1,12 @@
+﻿namespace kit_api.Models
+{
+    public class Tiempos
+    {
+        public int Ticket { get; set; }
+        public int Folio { get; set; }
+        public string Asesor { get; set; }
+        public DateTime FechaSoporte { get; set; }
+        public float Tiempo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
